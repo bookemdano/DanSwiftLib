ffae27b51839d6a254e375b7a1288fa883cb23b6
Dano <bookemdano@gmail.com>
2025-03-02 09:50:18 -0500

    Better testing lib name.

--- Sources/DanSwiftLib/DanSwiftLib.swift
+++ Sources/DanSwiftLib/DanSwiftLib.swift
@@ -1,6 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-public struct MyLibrary {
+public struct DanSwiftLib {
     public static func hello() -> String {
         return "Hello from DanSwiftLib!"
     }
