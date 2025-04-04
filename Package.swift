7b27a75e963ccefca9e3ca16044d2489c8f211f0
Dano <bookemdano@gmail.com>
2025-04-04 15:18:42 -0400

    Add AWS to library.

--- Package.swift
+++ Package.swift
@@ -11,11 +11,20 @@ let package = Package(
             name: "DanSwiftLib",
             targets: ["DanSwiftLib"]),
     ],
+    dependencies: [
+        // Add the AWS SDK package dependency (adjust the URL and version as needed)
+        .package(
+            url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git",
+            from: "2.40.0"
+        ),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DanSwiftLib"),
-
+            name: "DanSwiftLib",
+            dependencies: [
+                           .product(name: "AWSS3", package: "aws-sdk-ios-spm")
+                       ]),
     ]
 )
--- Sources/DanSwiftLib/DanSwiftLib.swift
+++ Sources/DanSwiftLib/DanSwiftLib.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 public struct DanSwiftLib {
-    public static func hello() -> String {
-        return "Hello from DanSwiftLib!"
+   public static func hello() -> String {
+       return "Hello from DanSwiftLib!"
     }
 }
